92f19ace0c29823a0cc6255fe8c6e9b092211bd4
POO <melani.molina@epn.edu.ec>
2023-01-16 11:04:12 -0500

    Initial commit

---
+++ src/dialogo1.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;//Libreria swing
+
+public class dialogo1 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Bienvenido \n a \n java");
+
+    }
+}
